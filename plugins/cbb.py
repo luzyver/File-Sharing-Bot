33e4f48a2f799fb1a0f607296fb5c534df08292c
luzyver <91455306+luzyver@users.noreply.github.com>
2021-09-27 21:18:24 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -4,14 +4,13 @@ from pyrogram import __version__
 from bot import Bot
 from config import OWNER_ID
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
-from start import Bot
 
 @Bot.on_callback_query()
 async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "dana":
         await query.message.edit_text(
-            text = f"<b>Halo {first}, silahkan klik <a href='https://t.me/dagetluzyverbot/14/'>disini</a>!</b>",
+            text = f"<b>Silahkan klik <a href='https://t.me/dagetluzyverbot/14/'>disini</a>!</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
