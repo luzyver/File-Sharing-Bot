2b246d17126f535111cd1f562d10284ecb9cd822
luzyver <91455306+luzyver@users.noreply.github.com>
2021-09-27 21:01:03 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "dana":
         await query.message.edit_text(
-            text = f"<b>Halo {first}, silahkan klik <a href="https://t.me/dagetluzyverbot/14/">disini</a>!</b>",
+            text = f"<b>Halo {first}, silahkan klik <a href='https://t.me/dagetluzyverbot/14/'>disini</a>!</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
