d9807bc46f343c16fe3e11cf4c8b1e9488a48c69
luzyver <91455306+luzyver@users.noreply.github.com>
2021-09-27 20:29:26 +0700

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>Test",
+            text = f"<b>Halo {first}, kamu mau <code>Rp. 15.000</code> dana? Caranya gampang, kamu download Neo+ kemudian daftar dengan memasukkan kode undangan <code>JTLB22</code>. Jika sudah buka rekening, tunggu sampai pembukaan rekening sukses. Jika sudah upload buktinya disini dengan format\n<code>FOTO</code>\nNo Pendaftaran :\nNo Dana :\n\nCek secara berkala link File Sharingnya!",
             reply_markup = InlineKeyboardMarkup(
                 [
                     [
