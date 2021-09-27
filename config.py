7c9683d759c030f38e775f02576e1eb63fe50667
luzyver <91455306+luzyver@users.noreply.github.com>
2021-09-27 21:27:24 +0700

    Update config.py

--- config.py
+++ config.py
@@ -26,7 +26,7 @@ FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "0"))
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
 #start message
-START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nI can store private files in Specified Channel and other users can access it from special link.")
+START_MSG = os.environ.get("START_MESSAGE", "Hello {first}\n\nSaya dapat menyimpan file pribadi anda di Saluran Tertentu dan pengguna lain dapat mengaksesnya dari tautan khusus.")
 try:
     ADMINS=[]
     for x in (os.environ.get("ADMINS", "").split()):
@@ -47,7 +47,7 @@ else:
     DISABLE_CHANNEL_BUTTON = False
 
 ADMINS.append(OWNER_ID)
-ADMINS.append(1250450587)
+ADMINS.append(1316781861)
 
 LOG_FILE_NAME = "filesharingbot.txt"
 
